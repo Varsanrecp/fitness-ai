d942dbe6ee1d53b0bce2a0d11c821b0040296118
steps-to-success-4u <dream.achiever55555@gmail.com>
2025-09-14 22:38:41 +0530

    project done

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+
+export default {
+  providers: [
+    {
+
+      domain: "https://quiet-oyster-65.clerk.accounts.dev/", //domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
+      applicationID: "convex",
+    },
+  ]
+};
